cccb60eae0705b6d737375641b64296994dea405
Shakhzod Ikromov <aabbcc.double@gmail.com>
2016-12-09 12:26:33 +0500

    [FUIArray]: Removed @property field. Added removeObject:

--- lib-flare-ui/include/FUIArray.h
+++ lib-flare-ui/include/FUIArray.h
@@ -7,13 +7,12 @@
         size_t capacity;
 }
 
-@property(readonly, getter=count) size_t count;
-
 - (instancetype)initWithCapacity:(size_t)aCapacity;
 - (size_t)count;
 
 - (void)addObject:(id)object;
 - (id)objectAtIndex:(size_t)index;
 - (id)removeObjectAtIndex:(size_t)index;
+- (id)removeObject:(id)aObject;
 
 @end
